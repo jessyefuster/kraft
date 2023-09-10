5dae3d7ad40a5df38fbd08ab2534fe8d387d9bde
jessyefuster <dev.fuster.jessye@gmail.com>
2023-09-10 14:22:41 +0200

    reference correct tsconfig to jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,7 +4,15 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['__tests__/utils/'],
-  coveragePathIgnorePatterns: ['/node_modules/', '__tests__/utils/', 'src/server/migrations/']
+  coveragePathIgnorePatterns: ['/node_modules/', '__tests__/utils/', 'src/server/migrations/'],
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      // required due to custom location of tsconfig.json configuration file
+      // https://kulshekhar.github.io/ts-jest/docs/getting-started/options/tsconfig
+      { tsconfig: '__tests__/tsconfig.json' }
+    ],
+  }
 }
 
 export default config;
