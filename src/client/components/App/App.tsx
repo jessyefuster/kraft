4c775ebd39b687011c7af73236b61d22a415e3b1
jessyefuster <dev.fuster.jessye@gmail.com>
2023-09-10 17:18:22 +0200

    add theme

--- src/client/components/App/App.tsx
+++ src/client/components/App/App.tsx
@@ -1,18 +1,24 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import Home from '../../pages/Home/Home';
 import Login from '../../pages/Login/Login';
 import NotFound from '../../pages/NotFound/NotFound';
+import { themeOptions } from '../../theme/theme';
+
+const theme = createTheme(themeOptions);
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
