89202841e813e698d715ee45dd9348b6c6477cd8
jessyefuster <dev.fuster.jessye@gmail.com>
2023-09-10 17:19:32 +0200

    form styling

--- src/client/features/auth/LoginForm/LoginForm.tsx
+++ src/client/features/auth/LoginForm/LoginForm.tsx
@@ -1,18 +1,39 @@
-import { Button, TextField, styled } from '@mui/material';
+import { Box, Button, TextField, Typography, styled } from '@mui/material';
+
+import logo from '../../../assets/logo.svg';
+
+const Container = styled(Box)({
+  minWidth: '40%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center'
+});
+
+const Logo = styled('img')({
+  height: 50,
+  alignSelf: 'center'
+});
 
 const Form = styled('form')({
-  minWidth: '40%',
+  width: '400px',
+  maxWidth: '100%',
+  margin: 'auto',
   display: 'flex',
   flexDirection: 'column'
 });
 
 const LoginForm = () => {
   return (
-    <Form>
-      <TextField label="Nom d'utilisateur" variant="outlined" />
-      <TextField label="Mot de passe" variant="outlined" type="password" />
-      <Button variant="contained">Se connecter</Button>
-    </Form>
+    <Container padding={4}>
+      <Form>
+        <Logo src={logo} alt="Kraft" />
+        <Typography variant='h4' sx={{ mt: 3 }}>Hello !</Typography>
+        <TextField label="Nom d'utilisateur" sx={{ mt: 2 }}/>
+        <TextField label="Mot de passe" type="password" sx={{ mt: 2 }} />
+        <Button variant="contained" sx={{ mt: 3 }}>Se connecter</Button>
+      </Form>
+    </Container>
   );
 };
   
