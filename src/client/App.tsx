b36a11d0656465e13d1e44adbc1919eccbe3bc11
jessyefuster <dev.fuster.jessye@gmail.com>
2023-09-17 17:07:15 +0200

    store persistance

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,30 +1,33 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { Provider } from 'react-redux';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
-import { store } from './app/store';
+import { persistor, store } from './app/store';
+import PrivateOutlet from './features/Auth/components/PrivateOutlet/PrivateOutlet';
 import LoginPage from './features/Auth/pages/LoginPage';
 import HomePage from './pages/Home';
 import NotFoundPage from './pages/NotFound';
 import { themeOptions } from './theme/theme';
-import PrivateOutlet from './features/Auth/components/PrivateOutlet/PrivateOutlet';
 
 const theme = createTheme(themeOptions);
 
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <Router>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/" element={<PrivateOutlet />}>
-              <Route index element={<HomePage />} />
-            </Route>
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
+      <PersistGate persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/" element={<PrivateOutlet />}>
+                <Route index element={<HomePage />} />
+              </Route>
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </Router>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/client/app/store.ts
+++ src/client/app/store.ts
@@ -1,18 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import { api } from './api';
 import authSlice from '../features/Auth/store/slice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
 export const store = configureStore({
-  reducer: {
+  reducer: persistReducer(persistConfig, combineReducers({
     [api.reducerPath]: api.reducer,
-    [authSlice.name]: authSlice.reducer 
-  },
+    [authSlice.name]: authSlice.reducer
+  })),
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware)
 });
 
-export default store;
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
