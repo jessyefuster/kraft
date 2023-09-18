a533fd63bc25a024ba2f00b4797f3f773b6ffb97
jessyefuster <dev.fuster.jessye@gmail.com>
2023-09-18 22:58:39 +0200

    appbar style

--- src/client/components/layout/AppBar.tsx
+++ src/client/components/layout/AppBar.tsx
@@ -2,6 +2,7 @@ import { styled } from '@mui/material';
 
 import logo from '../../assets/logo_white.svg';
 import LogoutButton from '../../features/Auth/components/LogoutButton/LogoutButton';
+import { useAuth } from '../../features/Auth/hooks/useAuth';
 
 const Logo = styled('img')({ });
 
@@ -12,14 +13,17 @@ const Header = styled('header')(({ theme }) => ({
   justifyContent: 'space-between',
   minWidth: 30,
   padding: 20,
-  background: `linear-gradient(180deg, ${theme.palette.primary.main} 0%, ${theme.palette.secondary.main} 100%)`
+  background: `linear-gradient(180deg, ${theme.palette.primary.main} 0%, ${theme.palette.secondary.main} 100%)`,
+  color: '#fff'
 }));
 
 const AppBar = () => {
+  const { user } = useAuth();
+
   return (
     <Header>
       <Logo src={logo} alt="Kraft" />
-      <LogoutButton />
+      {user && <LogoutButton />}
     </Header>
   );
 };
