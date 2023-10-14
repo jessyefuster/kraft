cf881141cc6ccc5cf232e7fcc4238c9ebfa33d5a
jessyefuster <dev.fuster.jessye@gmail.com>
2023-10-14 12:33:36 +0200

    permissions on all API routes

--- apps/server/src/routes/users.ts
+++ apps/server/src/routes/users.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 
 import UsersController from '../controllers/users';
-import { isAuthenticated } from '../middlewares/auth';
+import { hasPermissions, isAuthenticated } from '../middlewares/auth';
 
 const router = Router();
 
-router.route('/:id').delete(isAuthenticated, UsersController.deleteOne);
-router.route('/').get(isAuthenticated, UsersController.getAll);
-router.route('/').post(UsersController.create);
+router.route('/:id').delete(isAuthenticated, hasPermissions(['DELETE_USERS']), UsersController.deleteOne);
+router.route('/').get(isAuthenticated, hasPermissions(['READ_USERS']), UsersController.getAll);
+router.route('/').post(isAuthenticated, hasPermissions(['CREATE_USERS']), UsersController.create);
 
 export default router;
