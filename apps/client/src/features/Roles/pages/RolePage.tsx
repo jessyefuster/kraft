c385e2bcb72719b875310fd3d49141b154e64dcb
jessyefuster <dev.fuster.jessye@gmail.com>
2023-10-28 00:40:10 +0200

    basic role info display

--- apps/client/src/features/Roles/pages/RolePage.tsx
+++ apps/client/src/features/Roles/pages/RolePage.tsx
@@ -1,23 +1,50 @@
-import { Box, styled } from '@mui/material';
+import ArrowBack from '@mui/icons-material/ArrowBack';
+import { styled } from '@mui/material';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useCallback } from 'react';
 
+import errorIllustrationUrl from '../../../assets/error_illustration.png';
+import { useGetRoleQuery } from '../../../app/api';
 import Page from '../../../components/layout/Page';
 
 const Header = styled(Box)({
   display: 'flex',
-  flexDirection: 'column'
+  flexDirection: 'column',
+  alignItems: 'start'
 });
 
 const RolePage = () => {
-  const { id } = useParams();
+  const navigate = useNavigate();
+  const { id = '' } = useParams();
+  const { data: role, isError } = useGetRoleQuery(id);
+
+  const onBackButtonClick = useCallback(() => navigate('/roles'), [navigate]);
 
   return (
     <Page padding={4}>
-      <Header>
-        <Typography variant="h4">Détail du rôle</Typography>
-        <Typography variant="overline">{id}</Typography>
-      </Header>
+      <Button
+        onClick={onBackButtonClick}
+        sx={{ padding: '8px', alignSelf: 'start' }}
+        startIcon={<ArrowBack />}
+      >
+        Retour aux rôles
+      </Button>
+      {role
+        ? (
+            <Header>
+              <Typography variant="h4">{role.name}</Typography>
+              <Typography variant="caption">ID du rôle <code>{id}</code></Typography>
+            </Header>
+          )
+        : isError && (
+            <Box sx={{ display: 'flex', flex: 1, justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
+              <img src={errorIllustrationUrl} height={350}/>
+            </Box>
+          )
+      }
     </Page>
   );
 };
